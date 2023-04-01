0bf125b03d38e6cd3335c086dd6f5f5f11df1ab0
karenrivera-tranetech <karen.rivera@tranetechnologies.com>
2023-04-01 15:10:52 -0600

    Added new learnOpt and learnPar files and adjusted makefile to build. Added new test data files and adjusted readme to capture expected output on Mac

---
+++ learnOpt.c
@@ -0,0 +1,149 @@
+/*
+ * learn.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "MT.h"
+#include "learn.h"
+#include "feature.h"
+
+void nmf_learn(double **data, int n_rows, int n_cols, int n_class, double **W, double **H, int maxiter){
+    double **X_hat;
+    int i,j;
+
+    X_hat = (double **)calloc(n_rows,sizeof(double *));
+    for(i = 0;i < n_rows;i++){
+        X_hat[i] = (double *)calloc(n_cols,sizeof(double));
+    }
+
+    // initialize W, H
+    init_genrand(2468);
+    // W(n_rows, n_class)
+    for(i = 0;i < n_rows;i++){
+        for(j = 0;j < n_class;j++){
+            W[i][j] = genrand_real3();
+        }
+    }
+    // H(n_class, n_cols)
+    for(i = 0;i < n_class;i++){
+        for(j = 0;j < n_cols;j++){
+            H[i][j] = genrand_real3();
+        }
+    }
+
+    // X_hat = W x H
+    int k;
+    for(i = 0;i < n_rows;i++){
+        for(j = 0;j < n_cols;j++){
+            X_hat[i][j] = 0.0;
+            for(k = 0;k < n_class;k++){
+                X_hat[i][j] += W[i][k] * H[k][j];
+            }
+        }
+    }
+
+
+    FILE *ofp;
+    if((ofp = fopen("ISD.txt","w")) == NULL){
+        fprintf(stderr,"nmf_learn:: cannnot open output file.\n");
+        exit(1);
+    }
+    fprintf(ofp,"STEP\tISD\n");
+
+
+    // iteration
+    int it;
+    double numerator;
+    double denominator;
+    double isd;
+    double prev_isd;
+    double converge_threshold = 1.0e-12;
+    double epsilon = 1.0e-12;
+    for(it = 0;it < maxiter;it++){
+        // compute IS divergence
+        isd = 0.0;
+        for(i = 0;i < n_rows;i++){
+            for(j = 0;j < n_cols;j++){
+                isd += ((data[i][j]+epsilon) / (X_hat[i][j]+epsilon)) - log((data[i][j]+epsilon) / (X_hat[i][j]+epsilon)) - 1.0;
+            }
+        }
+       // only ouput this value every 100 iterations
+        if(it % 500 == 0){
+            fprintf(stdout,"\nIS Divergence = %.8f\n",isd);
+            // printf("iteration %2d / %3d..\n",it+1,maxiter);
+            fflush(stdout);
+        }
+        if(it == maxiter - 1) {
+            fprintf(stdout,"\nIS Divergence = %.8f\n",isd);
+            fflush(stdout);
+        }
+        fprintf(ofp,"%d\t%.8f\n",it,isd);
+        if((it != 0) && ((prev_isd*1.05) < isd)) break;
+        if((it != 0) && (fabs(prev_isd - isd) < converge_threshold)){
+            printf("converged.\n");
+            break;
+        }
+        prev_isd = isd;
+
+        // update rules for minimizing IS divergence
+        // update W
+        for(i = 0;i < n_rows;i++){
+            for(k = 0;k < n_class;k++){
+                if(W[i][k] != 0.0){
+                    numerator = 0.0;
+                    denominator = 0.0;
+                    for(j = 0;j < n_cols;j++){
+                        numerator += ((data[i][j]+epsilon) * H[k][j]) / ((X_hat[i][j]+epsilon) * (X_hat[i][j]+epsilon));
+                        denominator += H[k][j] / (X_hat[i][j]+epsilon);
+                    }
+                    if(denominator != 0.0){
+                        W[i][k] = W[i][k] * sqrt(numerator / denominator);
+                    }else{
+                        W[i][k] = W[i][k] * sqrt(numerator / epsilon);
+                    }
+                    if(W[i][k] < epsilon) W[i][k] = 0.0;
+                }
+            }
+        }
+        // update X_hat
+        for(i = 0;i < n_rows;i++){
+            for(j = 0;j < n_cols;j++){
+                X_hat[i][j] = 0.0;
+                for(k = 0;k < n_class;k++){
+                    X_hat[i][j] += W[i][k] * H[k][j];
+                }
+            }
+        }
+        // update H
+        for(k = 0;k < n_class;k++){
+            for(j = 0;j < n_cols;j++){
+                if(H[k][j] != 0.0){
+                    numerator = 0.0;
+                    denominator = 0.0;
+                    for(i = 0;i < n_rows;i++){
+                        numerator += ((data[i][j]+epsilon) * W[i][k]) / ((X_hat[i][j]+epsilon) * (X_hat[i][j]+epsilon));
+                        denominator += W[i][k] / (X_hat[i][j]+epsilon);
+                    }
+                    if(denominator != 0.0){
+                        H[k][j] = H[k][j] * sqrt(numerator / denominator);
+                    }else{
+                        H[k][j] = H[k][j] * sqrt(numerator / epsilon);
+                    }
+                    if(H[k][j] < epsilon) H[k][j] = 0.0;
+                }
+            }
+        }
+        // update X_hat
+        for(i = 0;i < n_rows;i++){
+            for(j = 0;j < n_cols;j++){
+                X_hat[i][j] = 0.0;
+                for(k = 0;k < n_class;k++){
+                    X_hat[i][j] += W[i][k] * H[k][j];
+                }
+            }
+        }
+    }
+    fclose(ofp);
+}
--- test.sh
+++ test.sh
@@ -6,21 +6,27 @@ OUTPUT_FILE="test_output.txt"
 n_input=$1
 iteration_input=$2
 file_input=$3
+threshold_input=$4
 use_threading_input=$4 # 0 = no threading, 1 = threading
 
 # default n_input to 2
 if [ -z "$n_input" ]; then
- n_input="2"
+ n_input="6"
 fi
  
 # default iteration_input to 50
 if [ -z "$iteration_input" ]; then
- iteration_input="50"
+ iteration_input="10000"
 fi
 
 # default file_input to iris_test_data.data
 if [ -z "$file_input" ]; then
- file_input="iris_test_data.data"
+ file_input=./med_data.dat
+fi
+
+# default threshold_input to 2
+if [ -z "$threshold_input" ]; then
+ threshold_input="2"
 fi
 
 if [ "$use_threading_input" == "1" ]; then
@@ -32,12 +38,11 @@ fi
 for thread_cnt in "${iter_loop[@]}"
  do
   export OMP_NUM_THREADS="$thread_cnt"
-  echo "$OMP_NUM_THREADS threads for NMF N: $n_input, Iteration: $iteration_input" | tee -a $OUTPUT_FILE
+  echo "$OMP_NUM_THREADS threads for NMF N: $n_input, Iteration: $iteration_input, T: $threshold_input, file: $file_input" | tee -a $OUTPUT_FILE
   for iter in 1 2 3 4 5 6 7 # loop through 7 different iterations to get a good average of timing.
    do
-    ret=$(./nmf -N $n_input -I $iteration_input ./iris_test_data.data)
-    full_time=${ret#*$SUB_STR}
-    stripped_time=`echo $full_time | sed s/"$SUFFIX"//`
+    ret=$(./nmf -N $n_input -I $iteration_input -T $threshold_input $file_input)
+    stripped_time=`echo $ret | sed -e "s/.*${SUB_STR}//;s/${SUFFIX}.*//"`
     echo -n "${stripped_time}, " | tee -a $OUTPUT_FILE
    done
    echo "" | tee -a $OUTPUT_FILE
