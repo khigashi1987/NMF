57d05c2d7cfa5ed1904530eeba9d3189ae18064e
karenrivera-tranetech <karen.rivera@tranetechnologies.com>
2023-04-26 16:19:55 -0600

    Fixed test runner to work with new parameters and updated results in notebook for new version of test files

--- test.sh
+++ test.sh
@@ -7,19 +7,21 @@ n_input=$1
 iteration_input=$2
 file_input=$3
 threshold_input=$4
-use_threading_input=$4 # 0 = no threading, 1 = threading
+use_threading_input=$5 # 0 = no threading, 1 = threading
+block_size=$6
+program_name=$7
 
-# default n_input to 2
+# default n_input to 6
 if [ -z "$n_input" ]; then
  n_input="6"
 fi
  
-# default iteration_input to 50
+# default iteration_input to 10000
 if [ -z "$iteration_input" ]; then
  iteration_input="10000"
 fi
 
-# default file_input to iris_test_data.data
+# default file_input to med_data.dat
 if [ -z "$file_input" ]; then
  file_input=./med_data.dat
 fi
@@ -27,6 +29,16 @@ fi
 # default threshold_input to 2
 if [ -z "$threshold_input" ]; then
  threshold_input="2"
+fi
+
+# default block_size to 1
+if [ -z "$block_size" ]; then
+ block_size="1"
+fi
+
+# default program_name to nmf
+if [ -z "$program_name" ]; then
+ program_name="nmf"
 fi
 
 if [ "$use_threading_input" == "1" ]; then
@@ -38,10 +50,10 @@ fi
 for thread_cnt in "${iter_loop[@]}"
  do
   export OMP_NUM_THREADS="$thread_cnt"
-  echo "$OMP_NUM_THREADS threads for NMF N: $n_input, Iteration: $iteration_input, T: $threshold_input, file: $file_input" | tee -a $OUTPUT_FILE
+  echo "$OMP_NUM_THREADS threads for $program_name -N $n_input -I $iteration_input -T $threshold_input -B $block_size $file_input" | tee -a $OUTPUT_FILE
   for iter in 1 2 3 4 5 6 7 # loop through 7 different iterations to get a good average of timing.
    do
-    ret=$(./nmf -N $n_input -I $iteration_input -T $threshold_input $file_input)
+    ret=$(./$program_name -N $n_input -I $iteration_input -T $threshold_input -B $block_size $file_input)
     stripped_time=`echo $ret | sed -e "s/.*${SUB_STR}//;s/${SUFFIX}.*//"`
     echo -n "${stripped_time}, " | tee -a $OUTPUT_FILE
    done
